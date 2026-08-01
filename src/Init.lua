33a36a76c195aaeb0a9f7d15c9ab12a99393cb7e
JhonaDev139 <120608656+Jhona852804@users.noreply.github.com>
2026-08-01 15:05:37 -0300

    Update PrefabsId in Init.lua

--- src/Init.lua
+++ src/Init.lua
@@ -20,7 +20,7 @@ local ReGui = {
 
 	--// Configuration
 	Debug = false,
-	PrefabsId = 71968920594655,
+	PrefabsId = 138900238570571,
 	DefaultTitle = "ReGui",
 	ContainerName = "ReGui",
 	DoubleClickThreshold = 0.3,
